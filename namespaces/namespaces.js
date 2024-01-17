83996dc10ce8e36fe05c69e9fce5630af71ca6ba
Kochetov <kochetov.yevhen.dev@gmail.com>
2024-01-17 17:14:10 +0200

    finish project

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -9,11 +9,28 @@ const expressServer = app.listen(8001);
 
 const io = socketio(expressServer);
 
-io.on("connection", (socket) => {
+io.of("/").on("connection", (socket) => {
+  socket.join("chat");
+  // socket.join("adminChat");
+  // io.of("/").to("chat").emit("WeclomeToChatRoom", {});
+  // io.of("/")
+  //   .to("chat")
+  //   .to("chat2")
+  //   .to("admitChat")
+  //   .emit("WeclomeToChatRoom", {});
+  io.of("/admin").emit("userJoinedMainNS", "");
+
   console.log(socket.id, "has connected");
 
   socket.on("newMessageToServer", (dataFromClient) => {
     console.log("Data", dataFromClient);
-    io.emit("newMessageToClients", { text: dataFromClient.text });
+    io.of("/").emit("newMessageToClients", { text: dataFromClient.text });
   });
+});
+
+io.of("/admin").on("connection", (socket) => {
+  console.log(socket.id, "has joined /admin");
+  // socket.join("chat");
+  // io.of("/admin").emit("messageToClientsFromAdmin", {});
+  io.of("/admin").to("chat").emit("WelcomeToChatRoom", {});
 });
